0fc666fac9fbf772578db67dc6ac449dff794f34
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-10-07 12:44:09 +0300

    more tests on is_na

--- tests/testthat/test-is-true-false-na.R
+++ tests/testthat/test-is-true-false-na.R
@@ -23,6 +23,98 @@ test_that(
 ) 
 
 test_that(
+  "test is_na with a character input and no coercion returns true when not NA", 
+  {
+    x <- c("T", "F", "0", "1", "a", "NA", NA)
+    expected <- rep.int(c(FALSE, TRUE), c(6, 1))
+    actual <- is_na(x, coerce_to_logical = FALSE)
+    expect_equal(strip_attributes(actual), expected)
+    expect_equal(names(actual), as.character(x))
+    expect_equal(
+      cause(actual), 
+      noquote(rep.int(c("not missing", ""), c(6, 1)))
+    )
+  }
+) 
+
+test_that(
+  "test is_na with a character input and coercion returns true when not 'T' or 'F'", 
+  {
+    x <- c("T", "F", "0", "1", "a", "NA", NA)
+    expected <- rep.int(c(FALSE, TRUE), c(2, 5))
+    expect_warning(
+      actual <- is_na(x, coerce_to_logical = TRUE),
+      "Coercing x to class .logical.\\."
+    )
+    expect_equal(strip_attributes(actual), expected)
+    expect_equal(names(actual), as.character(x))
+    expect_equal(
+      cause(actual), 
+      noquote(rep.int(c("true", "false", ""), c(1, 1, 5)))
+    )
+  }
+) 
+
+test_that(
+  "test is_na with a complex input and no coercion returns true when not NA", 
+  {
+    x <- c(0, 1, 1i, 1 + 1i, NA)
+    expected <- rep.int(c(FALSE, TRUE), c(4, 1))
+    actual <- is_na(x, coerce_to_logical = FALSE)
+    expect_equal(strip_attributes(actual), expected)
+    expect_equal(names(actual), as.character(x))
+    expect_equal(
+      cause(actual), 
+      noquote(rep.int(c("not missing", ""), c(4, 1)))
+    )
+  }
+) 
+
+test_that(
+  "test is_na with a complex input and coercion returns true when not NA", 
+  {
+    x <- c(0, 1, 1i, 1 + 1i, NA)
+    expected <- rep.int(c(FALSE, TRUE), c(4, 1))
+    expect_warning(
+      actual <- is_na(x, coerce_to_logical = TRUE),
+      "Coercing x to class .logical.\\."
+    )
+    expect_equal(strip_attributes(actual), expected)
+    expect_equal(names(actual), as.character(x))
+    expect_equal(
+      cause(actual), 
+      noquote(rep.int(c("false", "true", ""), c(1, 3, 1)))
+    )
+  }
+) 
+
+test_that(
+  "test is_na with a list input and no coercion returns true when elements contain a single NA", 
+  {
+    x <- list(NA, TRUE, FALSE, c(TRUE, FALSE, NA))
+    expected <- rep.int(c(TRUE, FALSE), c(1, 3))
+    actual <- is_na(x, coerce_to_logical = FALSE)
+    expect_equal(strip_attributes(actual), expected)
+    expect_equal(names(actual), as.character(x))
+    expect_equal(
+      cause(actual), 
+      noquote(rep.int(c("", "not missing"), c(1, 3)))
+    )
+  }
+) 
+
+test_that(
+  "test is_na with a list input and coercion throw an error", 
+  {
+    x <- list(NA, TRUE, FALSE, c(TRUE, FALSE, NA))
+    expect_error(
+      is_na(x, coerce_to_logical = TRUE),
+      "x cannot be coerced to any of these types: .logical.\\."
+    )
+  }
+) 
+
+test_that(
   "test is_not_false with a logical input returns true when not false", 
   {
     x <- c(TRUE, FALSE, NA)
